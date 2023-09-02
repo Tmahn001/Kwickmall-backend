76c8c58d644c0fa101566c7eb6d0d0edfec27f83
Tmahn001 <toluiwalewa@gmail.com>
2023-09-02 06:23:02 +0100

    chat-changed

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 
--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from api.views import ChatViewSet, MessageViewSet
 from rest_framework import routers
-from api.views import ChatViewSet, MessageViewSet, ChatMessageListView, CloseChatView, SellerChatMessageListView, SellerOpenChatListView, AdminChatMessageListView, AdminOpenChatListView, BuyerChatMessageListView, BuyerOpenChatListView
+from api.views import ChatViewSet, MessageViewSet, ChatMessageListView, CloseChatView, SellerChatMessageListView, SellerOpenChatListView, AdminChatMessageListView, AdminOpenChatListView, BuyerChatMessageListView, BuyerOpenChatListView, OpenChatCount
 from django.conf import settings
 from django.conf.urls.static import static
 from api import consumers
@@ -43,4 +43,5 @@ urlpatterns = [
     path('api/seller/<str:seller_id>/chats/<uuid:chat_id>/messages/', SellerChatMessageListView.as_view(), name='seller-chat-messages'),
     path('api/buyer/<str:buyer_id>/chats/<uuid:chat_id>/messages/', BuyerChatMessageListView.as_view(), name='buyer-chat-messages'),
     path('api/admin/chats/<uuid:chat_id>/messages/', AdminChatMessageListView.as_view(), name='admin-chat-messages'),
+    path('api/open-chat/', OpenChatCount.as_view(), name='open-unresolved-chat-count'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
